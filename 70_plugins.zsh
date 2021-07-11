b7efe2af42e412c72d47b2d00d27b07419ca28fd
Ethan Djeric <ethan@ethandjeric.com>
2021-07-11 18:27:15 +1000

    inital commit

---
+++ 70_plugins.zsh
@@ -0,0 +1,17 @@
+source /home/ethan/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-powerlevel10k/powerlevel10k.zsh-theme
+source /home/ethan/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-powerlevel10k/powerlevel9k.zsh-theme
+fpath+=( /home/ethan/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-powerlevel10k )
+source /home/ethan/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-Aloxaf-SLASH-fzf-tab/fzf-tab.plugin.zsh
+fpath+=( /home/ethan/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-Aloxaf-SLASH-fzf-tab )
+source /home/ethan/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
+fpath+=( /home/ethan/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions )
+source /home/ethan/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-SLASH-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
+fpath+=( /home/ethan/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-SLASH-fast-syntax-highlighting )
+source /home/ethan/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/gitignore/gitignore.plugin.zsh
+fpath+=( /home/ethan/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/gitignore )
+source /home/ethan/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/command-not-found/command-not-found.plugin.zsh
+fpath+=( /home/ethan/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/command-not-found )
+source /home/ethan/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-theunraveler-SLASH-zsh-fancy_ctrl_z/fancy_ctrl_z.plugin.zsh
+fpath+=( /home/ethan/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-theunraveler-SLASH-zsh-fancy_ctrl_z )
+source /home/ethan/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search/zsh-history-substring-search.plugin.zsh
+fpath+=( /home/ethan/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search )
---
+++ 90_final.zsh
@@ -0,0 +1,5 @@
+enable-fzf-tab
+echo "\n$fg[red]CALENDAR$reset_color"
+khal list today 3d
+echo "\n$fg[red]TODO$reset_color"
+cat ~/.local/share/cmdcache/todoman
